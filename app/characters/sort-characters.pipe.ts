4955f84e868ee49c80a381b6a3822177df31732a
Piotr Kościelniak <piotrek.koscielniak@gmail.com>
2016-03-07 22:34:37 +0100

    character component

--- app/characters/sort-characters.pipe.ts
+++ app/characters/sort-characters.pipe.ts
@@ -1,3 +1,20 @@
-/**
- * Created by piotr on 06.03.16.
- */
+import {Pipe,PipeTransform} from "angular2/core";
+
+@Pipe({name: 'sortCharacters'})
+export class SortCharactersPipe implements PipeTransform {
+
+    transform(value:any, args:any[]):any {
+        if (!value || !value.sort) {
+            return value;
+        }
+        return value.sort((a:Character, b:Character) => {
+            if (a.name < b.name) {
+                return -1;
+            }
+            if (a.name > b.name) {
+                return 1;
+            }
+            return 0;
+        });
+    }
+}
